34764ea2cf1a52f76e16654506ffcbccec79b892
Alexander <alexander-kilyushin@gmail.com>
2021-09-07 23:17:15 +0300

    add grid layout

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ type Page struct {
 var templates = template.Must(template.ParseFiles("index.html"))
 
 func renderPage(path string) (*Page, error) {
-	files, err := ioutil.ReadDir("./files")
+	files, err := ioutil.ReadDir("./files/" + path)
 
 	if err != nil {
 		log.Fatal(err)
@@ -39,7 +39,7 @@ func renderPage(path string) (*Page, error) {
 		if file.IsDir() {
 			folders = append(folders, Folder{Name: file.Name()})
 		} else {
-			content, err := ioutil.ReadFile("./files/" + file.Name())
+			content, err := ioutil.ReadFile("./files/" + path + "/" + file.Name())
 
 			if err != nil {
 				return nil, err
@@ -52,8 +52,8 @@ func renderPage(path string) (*Page, error) {
 	return &Page{Folders: folders, Path: path, SvgFiles: svgFiles}, nil
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, page *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", page)
+func renderTemplate(w http.ResponseWriter, page *Page) {
+	err := templates.ExecuteTemplate(w, "index.html", page)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -61,7 +61,7 @@ func renderTemplate(w http.ResponseWriter, tmpl string, page *Page) {
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		m := regexp.MustCompile("^/(files)/([a-zA-Z0-9]+)$").FindStringSubmatch(r.URL.Path)
+		m := regexp.MustCompile("^/(files)/([a-zA-Z0-9/-]+)?$").FindStringSubmatch(r.URL.Path)
 		if m == nil {
 			http.NotFound(w, r)
 			return
@@ -75,16 +75,11 @@ func explorerHandler(w http.ResponseWriter, r *http.Request, path string) {
 	if err != nil {
 		return
 	}
-	renderTemplate(w, "index", renderedPage)
-}
-
-func heheHandler(w http.ResponseWriter, r *http.Request, path string) {
-	http.Error(w, "error ;) hhe", http.StatusInternalServerError)
+	renderTemplate(w, renderedPage)
 }
 
 func main() {
 	http.HandleFunc("/files/", makeHandler(explorerHandler))
-	http.Handle("/hehe", makeHandler(heheHandler))
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(""))))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
